76a6b3632f0ff43a0d089f544f1622786db87196
Adrian Gabriel Stan <adrian.stan@librabank.ro>
2021-12-10 13:20:39 +0200

    Am adaugat back office fara controller

---
+++ ProgrammingClub/Repositories/BackOfficeRepository.cs
@@ -0,0 +1,29 @@
+﻿using ProgrammingClub.Models.DBObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProgrammingClub.Repositories
+{
+    public class BackOfficeRepository
+    {
+
+        private ClubMembershipModelsDataContext dbContext;
+
+        public BackOfficeRepository()
+        {
+            this.dbContext = new ClubMembershipModelsDataContext();
+        }
+
+        public BackOfficeRepository(ClubMembershipModelsDataContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+
+
+
+    }
+}
